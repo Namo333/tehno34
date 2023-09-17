38b38af4049c1c5085f8da2adcfa4f9a5d274c03
Namo333 <dad688634@gmail.com>
2023-09-17 12:06:40 +0300

    reset

--- my-app/src/routes/+layout.svelte
+++ my-app/src/routes/+layout.svelte
@@ -1,20 +1,15 @@
 <script>
-	import Header from '../lib/components/Header.svelte';
-	import Navmenu from '../lib/components/Navmenu.svelte';
-	import Footer from '../lib/components/Footer.svelte';
 	import '../lib/styles.css';
 	import '../lib/adaptive.css';
 </script>
 
 <div class="app">
-	<Header />
-	<Navmenu />
+
 
 	<main>
 		<slot />
 	</main>
 
-	<Footer />
 </div>
 
 <style>
