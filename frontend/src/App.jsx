388c42ebaf55b95a7316d4484cfc46d636630cc3
Namo333 <dad688634@gmail.com>
2023-09-23 10:42:28 +0300

    add routs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import { Navbar } from './components';
+
+import Home from './pages/Home';
+import About from './pages/About';
 
 const App = () => (
   <div className="w-full overflow-hidden">
-    <div className="">
-      <p>app</p>
-    </div>
+      <div className="">
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+        </Switch>
+      </div>
   </div>
 )
 
---
+++ frontend/src/components/index.js
@@ -0,0 +1,10 @@
+import Navbar from "./navbar";
+import Hero from "./Hero";
+import Footer from "./footer";
+
+export{
+    Navbar,
+    Hero,
+    Footer,
+}
+
